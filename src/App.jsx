754e001f983be48bf1bd909162ad67bc6865324f
niki2422 <randazzonico55@gmail.com>
2026-03-16 10:43:48 -0300

    se creo la seccion about con su respectivo .css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Services from "./components/Services"
 import Gallery from "./components/Gallery"
+import About from "./components/About"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Services />
       <Gallery />
+      <About />
     </>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+import "../styles/about.css"
+
+function About(){
+
+return(
+
+<section id="about" className="about">
+
+<h2>Sobre nosotros</h2>
+
+<p>
+La Dra. Marta Fernandez, matrícula 123xxx24234,
+cuenta con años de experiencia brindando atención odontológica
+de calidad en Bahia Blanca.
+</p>
+
+</section>
+
+)
+
+}
+
+export default About
