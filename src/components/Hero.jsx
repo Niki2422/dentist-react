a0c66045895360be6b13623af8387cf96342660c
niki2422 <randazzonico55@gmail.com>
2026-03-16 10:14:32 -0300

    se creo el componente Hero con su respectivo .css

---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import "../styles/Hero.css"
+
+function Hero(){
+
+return(
+
+<section className="hero">
+
+<div className="hero-content">
+
+<h1>Sonríe con confianza</h1>
+
+<p>
+Atención odontológica profesional con la Dra. Marta Fernandez
+</p>
+
+<a href="#contact" className="hero-btn">
+Agendar turno
+</a>
+
+</div>
+
+</section>
+
+)
+
+}
+
+export default Hero
