229c743a6d80a2e2d4892a1ee646bdeff5e68364
niki2422 <randazzonico55@gmail.com>
2026-03-16 11:10:34 -0300

    se creo la seccion footer con su respectivo .css

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Gallery from "./components/Gallery"
 import About from "./components/About"
 import Testimonials from "./components/Testimonials"
 import Contact from "./components/Contact"
+import Footer from "./components/Footer"
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <About />
       <Testimonials />
       <Contact />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import "../styles/Footer.css"
+
+function Footer(){
+
+return(
+
+<footer className="footer">
+
+<p>
+DentistReact - Dra Marta Fernandez
+</p>
+
+<p>
+Bahia Blanca - Argentina
+</p>
+
+</footer>
+
+)
+
+}
+
+export default Footer
